f89c740abf811bbab42569597ae7a5f1b987cf3d
Jakob Flierl <koppi@users.noreply.github.com>
2021-01-08 14:12:43 +0100

    Update copyright time

--- rm501.c
+++ rm501.c
@@ -1,7 +1,7 @@
 /*
  * rm501.c - Mitsubishi RM-501 Movemaster II Robot Simulator
  *
- * Copyright (C) 2013-2019 Jakob Flierl <jakob.flierl@gmail.com>
+ * Copyright (C) 2013-2021 Jakob Flierl <jakob.flierl@gmail.com>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
